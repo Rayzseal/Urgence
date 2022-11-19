df079c86a2ecf98fe5ab899bba30d97d296cd80f
Rayzseal <102787463+Rayzseal@users.noreply.github.com>
2022-11-20 00:21:42 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import back.Scheduler;
+import back.State;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scheduler s = new Scheduler();
+		s.run();
+	}
+
+}
