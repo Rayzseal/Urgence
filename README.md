3d4516ee3d98c5e26fe30472212a45637fee360d
Elllyyn <ellyn.lafontaine@outlook.fr>
2023-01-13 11:22:50 +0100

    read me

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Urgence
 4A projet urgence
+
+Pour lancer ce projet, il faudra modifier le build path des librairies utilisées.
+Ainsi, il vous faudra faire clique droit sur le projet puis build path, 
+puis configure build path.
+Il faudra ajouté les jar dans le classpath (pas le module path).
+
+Les jar à ajouter : 
+- jcommon-1.0.23.jar
+- jfreechart-1.0.19.jar
+- junit-4.11.jar
+
+Une fois fois cette configuration effectuée, vous pourrez lancer le projet en lançant 
+la classe MainApp dans le package hud.
--- src/hud/MainApp.java
+++ src/hud/MainApp.java
@@ -10,14 +10,13 @@ import controller.Scheduler;
 public class MainApp {
 
 	public static void main(String[] args) {
-
-		//Scheduler s = new Scheduler("PatientExemple.csv");
-		//Scheduler s = new Scheduler(DataFile.readDataFile("SimulationExemple.csv"));
-		Scheduler s = new Scheduler(50);
+		//read a file of patients of 50 patients
+		Scheduler s = new Scheduler("PatientExemple.csv");
+		//generate 50 patients randomly
+		//Scheduler s = new Scheduler(50);
 
 		s.run();
 		
-		//HUDMain hud = new HUDMain(DataFile.readDataFile("SimulationEnd.csv"));
 		HUDMain hud = new HUDMain(s.getData());
 		hud.setFrame();
 
