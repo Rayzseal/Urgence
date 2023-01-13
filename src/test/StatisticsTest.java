72cbdd80bb449ba3d5060f0f2179101cfe706b9c
Rayzseal <chloe.duault@laposte.net>
2023-01-13 01:36:41 +0100

    des tests unitaires

--- src/test/StatisticsTest.java
+++ src/test/StatisticsTest.java
@@ -1,12 +1,88 @@
 package test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import controller.Statistics;
+import model.State;
+import utils.DataFile;
 
 public class StatisticsTest {
 
+	/**
+	 * Test on average spent time waiting. 
+	 */
 	@Test
-	 public void testAverage() {
-		
+	public void testAverage() {
+		int average = Statistics.getAverageWaitingTime(DataFile.readDataFile("SimulationEndTest.csv"));
+		assertEquals(average, 87);
+	}
+	
+	/**
+	 * Test on average spent time in emergency. 
+	 */
+	@Test
+	public void testAverageSpendTimeEmergency() {
+		double average = Statistics.getAverageTimeSpentInEmergency(DataFile.readDataFile("SimulationEndTest.csv"));
+		assertEquals(average, 4047.7);
+	}
+	
+	/**
+	 * Test on percentage of utilization of each states. 
+	 */
+	@Test
+	public void percentageUtilizationReception() {
+		Map<State, Double> mapPercentageBedroom = 
+		Statistics.getPercentageUtilizationStates(DataFile.readDataFile("SimulationEndTest.csv"));
+		double percentageBedroom = mapPercentageBedroom.get(State.RECEPTION);
+		assertEquals(percentageBedroom, (double)15.851768352825138);
+	}
+	
+	/**
+	 * Test on percentage of utilization of each states. 
+	 */
+	@Test
+	public void percentageUtilizationBloc() {
+		Map<State, Double> mapPercentageBedroom = 
+		Statistics.getPercentageUtilizationStates(DataFile.readDataFile("SimulationEndTest.csv"));
+		double percentageBedroom = mapPercentageBedroom.get(State.BLOC);
+		assertEquals(percentageBedroom, (double)3.170353670565027);
+	}
+	
+	/**
+	 * Test on percentage of utilization of each states. 
+	 */
+	@Test
+	public void percentageUtilizationScanner() {
+		Map<State, Double> mapPercentageBedroom = 
+		Statistics.getPercentageUtilizationStates(DataFile.readDataFile("SimulationEndTest.csv"));
+		double percentageBedroom = mapPercentageBedroom.get(State.SCANNER);
+		assertEquals(percentageBedroom, (double)6.340707341130054);
+	}
+	
+	/**
+	 * Test on percentage of utilization of each states. 
+	 */
+	@Test
+	public void percentageUtilizationPrescription() {
+		Map<State, Double> mapPercentageBedroom = 
+		Statistics.getPercentageUtilizationStates(DataFile.readDataFile("SimulationEndTest.csv"));
+		double percentageBedroom = mapPercentageBedroom.get(State.PRESCRIPTION);
+		assertEquals(percentageBedroom, (double)15.851768352825138);
+	}
+	
+	/**
+	 * Test on percentage of utilization of each states. 
+	 */
+	@Test
+	public void percentageUtilizationAnalysis() {
+		Map<State, Double> mapPercentageBedroom = 
+		Statistics.getPercentageUtilizationStates(DataFile.readDataFile("SimulationEndTest.csv"));
+		double percentageBedroom = mapPercentageBedroom.get(State.ANALYSIS);
+		assertEquals(percentageBedroom, (double)1.5851768352825135);
 	}
 
 }
--- src/test/UtilsTest.java
+++ src/test/UtilsTest.java
@@ -1,8 +1,7 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import utils.Utils;
 /**
@@ -11,30 +10,42 @@ import utils.Utils;
  */
 public class UtilsTest {
 
-	@Test
 	/**
 	 * Test on number of 0
 	 */
+	@Test
 	public void testPrintTime() {
 		int time = 360;
 		assertEquals("00:06:00", Utils.timeIntToString(time));
 	}
-	@Test
+	
 	/**
 	 * Test on a random time
 	 */
+	@Test
 	public void testPrintTime2() {
 		int time = 33366;
 		assertEquals("09:16:06", Utils.timeIntToString(time));
 	}
 	
-	@Test
 	/**
 	 * Test on the time with more than a day
 	 */
+	@Test
 	public void testPrintTime3() {
 		int time = 86400;
 		System.out.println(Utils.timeIntToString(time));
 		assertEquals("1 day(s) 00:00:00", Utils.timeIntToString(time));
 	}
+	
+	/**
+	 * Test on the time using max int value
+	 */
+	@Test
+	public void testPrintTime4() {
+		int time = 2147483647;
+		System.out.println(Utils.timeIntToString(time));
+		assertEquals("24855 day(s) 03:14:07", Utils.timeIntToString(time));
+	}
+	
 }
